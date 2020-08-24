26da8f4d48c2841c88299f92b42424e4058ad663
Santosh Parakh <santoshparakh@Santoshs-MacBook-Air.local>
2020-08-24 12:22:36 +0530

    This is initial xcode project

---
+++ youtube_oneDay/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtube_oneDay
+//
+//  Created by Santosh Parakh on 24/08/20.
+//  Copyright © 2020 Santosh Parakh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
